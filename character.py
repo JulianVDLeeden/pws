b4c90b011cefa0af7acd9fba691479ae8ae59e04
JulianVDLeeden <157350047+JulianVDLeeden@users.noreply.github.com>
2024-08-27 15:45:47 +0000

    spike(wat veel te moelijk was) en player1 game over (player2 nog niet)

--- character.py
+++ character.py
@@ -14,6 +14,8 @@ font = pygame.font.SysFont('default', 64)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.FULLSCREEN)
 fps_clock = pygame.time.Clock()
 
+spike_group = pygame.sprite.Group()
+
 class World():
 
   def __init__(self, data):
@@ -41,6 +43,10 @@ class World():
           img_rect.y = row_count * tile_size
           tile = (img, img_rect)
           self.tile_list.append(tile)
+        elif tile == 3:
+          spike = Spike(col_count * tile_size, row_count * tile_size)
+          spike_group.add(spike)
+
         col_count += 1
       row_count += 1
 
@@ -49,25 +55,37 @@ class World():
       screen.blit(tile[0], tile[1])
       pygame.draw.rect(screen, (255,255,255), tile[1], 2)
 
+
+class Spike(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('Picture/spike.png')
+        self.image = pygame.transform.scale(img, (tile_size, tile_size))  # Scale the image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+
+
 world_data = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 1],
+[1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
 world = World(world_data)
@@ -94,75 +112,78 @@ class Player_1():
     self.jump = False
     self.direction = 0
 
-  def update(self):
+  def update(self, game_over):
     dx = 0
     dy = 0
     walk_cooldown = 5
 
-    #keys inklikken
-    key = pygame.key.get_pressed()
-    if key[pygame.K_UP] and self.jump == False:
-      self.vel_y = -15
-      self.jump = True
-    if key[pygame.K_UP] == False:
-      self.jump = False
-    if key[pygame.K_LEFT]:
-      dx -= 5
-      self.counter += 1
-      self.direction = -1
-    if key[pygame.K_RIGHT]:
-      dx += 5
-      self.counter += 1
-      self.direction = 1
-    if key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] == False:
-      self.counter = 0
-      self.index = 0
-      if self.direction == 1:
-       self.image = self.image_right[self.index]
-      if self.direction == -1:
-       self.image = self.image_left[self.index]
+    if game_over == 0:
+      #keys inklikken
+      key = pygame.key.get_pressed()
+      if key[pygame.K_UP] and self.jump == False:
+        self.vel_y = -15
+        self.jump = True
+      if key[pygame.K_UP] == False:
+        self.jump = False
+      if key[pygame.K_LEFT]:
+        dx -= 5
+        self.counter += 1
+        self.direction = -1
+      if key[pygame.K_RIGHT]:
+        dx += 5
+        self.counter += 1
+        self.direction = 1
+      if key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] == False:
+        self.counter = 0
+        self.index = 0
+        if self.direction == 1:
+          self.image = self.image_right[self.index]
+        if self.direction == -1:
+          self.image = self.image_left[self.index]
 
-    #animation
-    if self.counter > walk_cooldown:
-      self.counter = 0
-      self.index += 1
-      if self.index >= len(self.image_right):
-        self.index = 0 
-      if self.direction == 1:
-       self.image = self.image_right[self.index]
-      if self.direction == -1:
-       self.image = self.image_left[self.index]
+      #animation
+      if self.counter > walk_cooldown:
+        self.counter = 0
+        self.index += 1
+        if self.index >= len(self.image_right):
+          self.index = 0 
+        if self.direction == 1:
+          self.image = self.image_right[self.index]
+        if self.direction == -1:
+          self.image = self.image_left[self.index]
 
-    #zwaartekracht
-    self.vel_y += 1
-    if self.vel_y > 10:
-      self.vel_y = 10
-    dy += self.vel_y  
-    
-    #Kijken of de player ergens tegenaan gaat
-    for tile in world.tile_list:
-      #x collision
-      if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
-        dx = 0
-      #y collision
-      if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
-        if self.vel_y < 0:
-          dy = tile[1].bottom - self.rect.top
-          self.vel_y = 0
-        elif self.vel_y >= 0:
-          dy = tile[1].top - self.rect.bottom  
-          self.vel_y = 0
+      #zwaartekracht
+      self.vel_y += 1
+      if self.vel_y > 10:
+        self.vel_y = 10
+      dy += self.vel_y  
+      
+      #Kijken of de player ergens tegenaan gaat
+      for tile in world.tile_list:
+        #x collision
+        if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+          dx = 0
+        #y collision
+        if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+          if self.vel_y < 0:
+            dy = tile[1].bottom - self.rect.top
+            self.vel_y = 0
+          elif self.vel_y >= 0:
+            dy = tile[1].top - self.rect.bottom  
+            self.vel_y = 0
 
-    #Player verplaatsen
-    self.rect.x += dx
-    self.rect.y += dy
+      #contact met spike
+      if pygame.sprite.spritecollide(self, spike_group, False):
+        game_over = -1
 
-    if self.rect.bottom > SCREEN_HEIGHT:
-      self.rect.bottom = SCREEN_HEIGHT
-      dy = 0
+      #Player verplaatsen
+      self.rect.x += dx
+      self.rect.y += dy
 
     screen.blit(self.image, self.rect)
     pygame.draw.rect(screen, (0, 0, 0), self.rect, 2)
+
+    return game_over
 
 
 class Player_2():
